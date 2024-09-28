3dfb49057c560969029a15e781619c2aca240301
fotouh <69843116+fotouh@users.noreply.github.com>
2024-09-28 06:47:00 +0300

    ..

--- assest/javascript/main.js
+++ assest/javascript/main.js
@@ -7869,7 +7869,7 @@ let mains = {
           </tr>
           <tr>
             <td>roaming_welcome_in	</td>
-            <td>Explain The Country Which </td>
+            <td>Explain The Country Which cs</td>
           </tr>
         </tbody>
       </table>`
